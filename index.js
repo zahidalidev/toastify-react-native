1de4f06c995febaea0e38496eb27055c01ac5729
zahidalidev <m.zahidalidev@gmail.com>
2021-05-15 01:25:49 +0500

    default props and formatted

--- index.js
+++ index.js
@@ -6,70 +6,48 @@ import Modal from 'react-native-modal';
 
 const { height, width } = Dimensions.get('window');
 export default class Toastify extends Component {
-  constructor() {
-    super();
-  }
 
   state = {
     isShow: false,
     text: '',
     opacityValue: new Animated.Value(1),
-    top: 50,
-    barWidth: new Animated.Value(0),
+    barWidth: new Animated.Value(RFPercentage(32)),
     containerWidth: RFPercentage(32),
+    backgroundColor: "#07bc0c",
+    textColor: "white"
   };
 
   show(duration) {
+    this.state.barWidth.setValue(this.state.containerWidth)  //reset barWidth value
     this.setState({
       isShow: true,
-      duration: typeof duration === 'number' ? duration : this.props.durationShort,
+      duration: duration,
     });
-
-    Animated.timing(this.state.opacityValue, {
-      toValue: this.props.opacity,
-      duration: this.props.fadeInDuration,
-      useNativeDriver: false,
-    }).start(() => {
-      this.isShow = true;
-      if (duration !== this.props.end) this.close();
-    });
-
-
+    this.isShow = true;
+    if (duration !== this.props.end) this.close(duration);
   }
 
   close(duration) {
-    let delay = typeof duration === 'number' ? duration : this.state.duration;
-    if (delay === this.props.end) delay = this.props.defaultCloseDelay;
-
     if (!this.isShow && !this.state.isShow) return;
     this.resetAll();
     setTimeout(() => {
-      Animated.timing(this.state.opacityValue, {
-        toValue: 0.0,
-        useNativeDriver: false,
-        duration: this.props.fadeOutDuration,
-      }).start(() => {
-        this.setState({ isShow: false });
-        this.isShow = false;
-      })
-
-    }, delay);
-
+      this.setState({ isShow: false });
+      this.isShow = false;
+    }, duration);
   }
 
   position() {
-    if (this.props.position === 'top') return this.state.top;
+    if (this.props.position === 'top') return this.props.positionValue;
     if (this.props.position === 'center') return height / 2;
-    return this.state.top;
+    return this.props.positionValue;
     // return (height / 2) - RFPercentage(10);
   }
 
   handleBar = () => {
     // Will change fadeAnim value to 1 in 5 seconds
-    this.state.barWidth.setValue(0)  //reset barWidth value
     Animated.timing(this.state.barWidth, {
-      toValue: this.state.containerWidth,
-      duration: (typeof duration === 'number' ? duration : this.state.duration) + 1000,
+      toValue: 0,
+      duration: this.state.duration,
       useNativeDriver: false,
     }).start();
   };
@@ -80,17 +58,24 @@ export default class Toastify extends Component {
 
   render() {
     this.handleBar();
-    return <Modal onModalHide={() => this.resetAll()} style={{ flex: 1, height, alignItems: "center" }} animationIn="slideInRight" animationOut="slideOutRight" isVisible={this.state.isShow} coverScreen={false} hasBackdrop={false} >
-      <View style={{ borderRadius: 5, position: "absolute", flexDirection: "column", alignItems: "flex-start", justifyContent: "center", width: this.state.containerWidth, height: RFPercentage(9), backgroundColor: "#07bc0c", top: this.position() }} >
-        <Text style={{ color: "white", marginLeft: RFPercentage(1), fontSize: RFPercentage(2.5) }} >Success</Text>
-        <View style={{
-          flexDirection: 'row',
-          height: 4,
-          width: '94%',
-        }}>
-          <Animated.View style={{ backgroundColor: "red", width: this.state.barWidth }} />
+    return (
+      <Modal onModalHide={() => this.resetAll()} style={{ flex: 1, height, alignItems: "center" }} animationIn="slideInRight" animationOut="slideOutLeft" isVisible={this.state.isShow} coverScreen={false} hasBackdrop={false} >
+        <View style={{ borderTopLeftRadius: 5, borderTopRightRadius: 5, position: "absolute", flexDirection: "column", alignItems: "flex-start", justifyContent: "center", width: this.state.containerWidth, height: RFPercentage(9), backgroundColor: this.state.backgroundColor, top: this.position() }} >
+          <Text style={{ color: this.state.textColor, marginLeft: RFPercentage(1), fontSize: RFPercentage(2.5) }} >Success</Text>
+          <View style={{ flexDirection: "row", position: "absolute", height: 4, width: '100%', bottom: 0 }}>
+            <Animated.View style={{ opacity: this.state.opacityValue, backgroundColor: "red", width: this.state.barWidth }} />
+          </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+    );
   }
 }
+
+Toastify.defaultProps = {
+  style: {},
+  position: 'top',
+  positionValue: 50,
+  opacity: 1,
+  defaultCloseDelay: 250,
+  end: 0,
+};
